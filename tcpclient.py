fdeea7c64b304996b13f4e15721c22602fe58b45
Jeremy Grosser <jgrosser@digg.com>
2009-07-12 17:09:24 -0700

    Playing around with streamer settings, adding a client for the tcp streamer

--- tcpclient.py
+++ tcpclient.py
@@ -18,15 +18,8 @@ audio = pa.open(format=pyaudio.paInt16,
                 output=True,
                 output_host_api_specific_stream_info=stream_info)
 
-sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-sock.bind(('', 9200))
-mreq = struct.pack('4sl', inet_aton('224.0.1.20'), INADDR_ANY)
-sock.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
-
-#decoder = '/usr/local/bin/lame --decode - -'
-decoder = '/bin/cat'
-
-p = Popen(decoder.split(' '), stdin=PIPE, stdout=PIPE)
+sock = socket()
+sock.connect(('127.0.0.1', 9201))
 
 def read_stream():
     while True:
@@ -36,17 +29,4 @@ def read_stream():
         #sys.stdout.write(data)
         #p.stdin.write(data)
 
-def play_stream():
-    while True:
-        audio.write(p.stdout.read(CHUNKSIZE))
-
-#t = Thread(target=read_stream)
-#t.setDaemon(True)
-#t.start()
-
 read_stream()
-
-#t1 = Thread(target=play_stream)
-#t1.setDaemon(True)
-#t1.start()
-
